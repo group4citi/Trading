f1ea9d79e0d23985ae607702ffd748117bc092e6
shsankar4 <shsankar4@gmail.com>
2020-10-09 06:14:10 +0000

    first commit

---
+++ src/main/java/com/example/demo/classes/Trade.java
@@ -0,0 +1,53 @@
+package com.example.demo.classes;
+
+import java.util.Date;
+
+public class Trade {
+	private Date dateCreated;
+	private String tradeID;
+	private String stockTicker;
+	private int quantity;
+	private double price;
+	public Trade( String stockTicker, int quantity, double price) {
+		this.dateCreated = new Date();
+		this.stockTicker = stockTicker;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	void createTrade() {
+		
+	}
+	void updateTrade() {
+		
+	}
+	void readTrade() {
+	
+	}
+	void deleteTrade() {
+	
+	}
+	public Date getDate() {
+		return dateCreated;
+	}
+	public void setDate(Date date) {
+		this.dateCreated = date;
+	}
+	public String getStockTicker() {
+		return stockTicker;
+	}
+	public void setStockTicker(String stockTicker) {
+		this.stockTicker = stockTicker;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ src/main/java/com/example/demo/classes/Users.java
@@ -0,0 +1,29 @@
+package com.example.demo.classes;
+
+public class Users {
+	private String emailID;
+	private String password;
+	public Users(String emailID, String password) {
+		this.emailID = emailID;
+		this.password = password;
+	}
+	void storeDetails() {
+		
+	}
+	void verifyCredentials() {
+		
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
